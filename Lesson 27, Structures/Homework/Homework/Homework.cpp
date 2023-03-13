e74d125f0267fe7ce7a2069474751bf93339a3f3
Alae <azzaoui@outlook.com>
2023-03-13 15:41:11 +0100

    C++ Level 1

--- Lesson 27, Structures/Homework/Homework/Homework.cpp
+++ Lesson 27, Structures/Homework/Homework/Homework.cpp
@@ -32,8 +32,16 @@ int main()
     Person1.Contact.Tel = "06 22 43 73 87";
 
     cout << "************************\n";
-    cout << "Name : " << Person1.name << "\nAge :" << Person1.age << "\nCity : " << Person1.city << "\nCountry : " << Person1.country << "\nMontly Salary : " <<
-        Person1.MontlySalary << "\nYearly Salary : " << Person1.YearlySalary << "\nGender : " << Person1.Gender << "\nStatus : " << Person1.Married
-        << "\nAddres : " << Person1.Contact.Addres << "\nEmail : " << Person1.Contact.Email << "\nTel : " << Person1.Contact.Tel << endl;
+    cout << "Name : " << Person1.name;
+    cout << "\nAge :" << Person1.age;
+    cout << "\nCity : " << Person1.city;
+    cout << "\nCountry : " << Person1.country;
+    cout << "\nMontly Salary : " << Person1.MontlySalary;
+    cout << "\nYearly Salary : " << Person1.YearlySalary;
+    cout << "\nGender : " << Person1.Gender;
+    cout << "\nStatus : " << Person1.Married;
+    cout << "\nAddres : " << Person1.Contact.Addres;
+    cout << "\nEmail : " << Person1.Contact.Email;
+    cout << "\nTel : " << Person1.Contact.Tel;
     cout << "************************\n";
 }
